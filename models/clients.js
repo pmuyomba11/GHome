0d0ff9446a9a4448730f64e93f975f88e971b4ca
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-07-13 16:55:38 -0400

    models and views created

---
+++ models/clients.js
@@ -0,0 +1,39 @@
+const users = [{
+    id: '1',
+    name:'mario',
+    age: 30
+    
+},
+{
+    id: '2',
+    name: 'luigi',
+    age: 45
+},
+{
+    id: '3',
+    name: 'Peter',
+    age: 27
+},
+{
+    id: '4' ,
+    name : "Yoshi",
+    age   : 20
+},
+{
+    id  :"5",
+    name:"Wario",
+    age  :  32
+},
+{
+    id     :6,
+    name   :"Bowser",
+    age    : 22
+},
+{
+    id      :7,
+    name    :"Toad",
+    age     :56
+}
+]
+
+module.exports = users
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const users = require('./models/clients')
 const colors = require('colors')
 require('dotenv').config();
 const morgan = require('morgan');
@@ -11,8 +12,37 @@ app.use(morgan('dev')) //Logging
 app.use(express.urlencoded({extended:true}));
 
 
+//Routes////////////////
+//Index Route
+app.get('/notes',(req,res) => {
+    res.render('index.ejs', {
+        allUsers : users
+    })
+});
 
-//Listening///
+//Show Route
+app.get('/notes/:id', (req,res) => {
+    res.send(users[req.params.id])
+})
+
+
+//Create Route
+app.post('/notes/', (req,res)=> {
+    res.send('Create Route')
+})
+
+
+//Update Route
+app.put('/notes', (req,res) => {
+    res.status(204).end()
+})
+
+//Delete Route
+app.delete('/notes/:id', (req,res) => {
+    res.redirect('/notes')
+})
+
+//PORT Listening///
 app.listen(PORT, ()=> {
     console.log(`Server running on Port: ${PORT}`.blue.bold.underline);
 })
