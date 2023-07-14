9cb7e734f24be39506e0d5672fd24dd134796a7a
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-07-13 20:05:56 -0400

    create route created

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const app = express();
 const users = require('./models/clients')
 const colors = require('colors')
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 //////////MIDDLEWARE///////
 app.use(morgan('dev')) //Logging
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 
 //Routes////////////////
@@ -20,26 +22,29 @@ app.get('/notes',(req,res) => {
     })
 });
 
+//New Route
+app.get('/notes/new', (req,res) => {
+    res.render('new.ejs')
+})
+
 //Show Route
 app.get('/notes/:id', (req,res) => {
-    res.send(users[req.params.id])
+    res.render('show.ejs', {
+        user: users[req.params.id]
+    })
 })
 
 
 //Create Route
-app.post('/notes/', (req,res)=> {
-    res.send('Create Route')
+app.post('/notes', (req,res) => {
+    users.push(req.body)
+    res.redirect('/notes')
 })
-
 
 //Update Route
 app.put('/notes', (req,res) => {
-    res.status(204).end()
-})
-
-//Delete Route
-app.delete('/notes/:id', (req,res) => {
-    res.redirect('/notes')
+    users.push(req.body)
+   res.redirect('/notes')
 })
 
 //PORT Listening///
